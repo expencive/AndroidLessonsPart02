404bf81fc8525da1ad93974156790bbd6a8fae06
expencive <43270198+expencive@users.noreply.github.com>
2018-09-21 15:21:47 +0300

    заготовка под свое приложение

---
+++ p0411layoutinflaterlist/src/main/java/ru/startandroid/p0411layoutinflaterlist/MainActivity.java
@@ -0,0 +1,51 @@
+package ru.startandroid.p0411layoutinflaterlist;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] name = { "01.", "02.", "03.", "04.", "05.", "06.",
+            "07.", "08." };
+    String[] position = { "Основной долг: 19378", "Основной долг: 19000", "Основной долг: 18600",
+            "Основной долг: 18000", "Основной долг: 17580", "Основной долг: 16300", "Основной долг: 1500", "Основной долг: 14590" };
+    int salary[] = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 };
+
+    int[] colors = new int[2];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        colors[0] = Color.parseColor("#FFECEDF6");
+        colors[1] = Color.parseColor("#FFECEDF6");
+
+
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        for (int i = 0; i < name.length; i++) {
+
+            View item = ltInflater.inflate(R.layout.item, linLayout, false);
+            TextView tvName = (TextView) item.findViewById(R.id.tvName);
+            tvName.setText(name[i]);
+            TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
+            tvPosition.setText(position[i]);
+            TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
+            tvSalary.setText("Оклад: " + String.valueOf(salary[i]));
+            item.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+            item.setBackgroundColor(colors[i % 2]);
+            linLayout.addView(item);
+        }
+
+    }
+}
