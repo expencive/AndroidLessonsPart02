7c1ae39b20533e01a35c1f953c19c3a8eb078945
expencive <43270198+expencive@users.noreply.github.com>
2018-09-25 11:13:42 +0300

    Доделал Список задач

--- mp031todolist/src/main/java/ru/startandroid/mp031todolist/MainActivity.java
+++ mp031todolist/src/main/java/ru/startandroid/mp031todolist/MainActivity.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -38,15 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         updateUI();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Пока еще не придумал как сделать", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
 
-            }
-        });
     }
 
     @Override
